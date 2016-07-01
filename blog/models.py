a946bda125307e4704cef38c63c1bfaa0d02403d
Maritza <maritzaesparza05@gmail.com>
2016-07-01 14:57:15 -0500

    Added tags and a recommendation for similar posts

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
 
@@ -27,6 +28,7 @@ class Post(models.Model):
     # The default manager
     objects = models.Manager()
     published = PublishedManager()
+    tags = TaggableManager()
 
     def get_absolute_url(self):
         return reverse('blog:post_detail',
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,8 @@ from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.generic import ListView
 from .models import Post, Comment
+from taggit.models import Tag
+from django.db.models import Count
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -12,8 +14,13 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
     paginator = Paginator(object_list, 3) # 3 posts in each page
     page = request.GET.get('page')
     try:
@@ -26,7 +33,8 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html',
                             {'page': page,
-                             'posts': posts})
+                             'posts': posts,
+                             'tag': tag})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
@@ -49,9 +57,17 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                  .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                 .order_by('-same_tags', '-publish')[:4]
     return render(request, 'blog/post/detail.html', {'post': post,
                                                      'comments': comments,
-                                                     'comment_form': comment_form})
+                                                     'comment_form': comment_form,
+                                                     'similar_posts': similar_posts})
 
 def post_share(request, post_id):
     # Retrieve post by id

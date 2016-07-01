79266ae0d9fa36e8a64eeb0e0c933945bc277e3c
Maritza <maritzaesparza05@gmail.com>
2016-07-01 01:30:12 -0500

    Added email and comments functionality

--- mysite/settings.py
+++ mysite/settings.py
@@ -101,3 +101,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Email configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'maritzaesparza05@gmail.com'
+EMAIL_HOST_PASSWORD = 'pistacheLinux1114'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
